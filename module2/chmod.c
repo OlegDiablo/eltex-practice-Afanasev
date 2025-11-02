b16dbe5267b060ed88db1179d369749676b6ba1e
OlegDiablo <afanev_maks00@mail.ru>
2025-11-03 04:45:24 +0700

    chmod commit

---
+++ module2/chmod.c
@@ -0,0 +1,228 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void printMask(mode_t mask){
+
+    printf((mask & S_IRUSR) ? "r" : "-");
+    printf((mask & S_IWUSR) ? "w" : "-");
+    printf((mask & S_IXUSR) ? "x" : "-");
+    printf((mask & S_IRGRP) ? "r" : "-");
+    printf((mask & S_IWGRP) ? "w" : "-");
+    printf((mask & S_IXGRP) ? "x" : "-");
+    printf((mask & S_IROTH) ? "r" : "-");
+    printf((mask & S_IWOTH) ? "w" : "-");
+    printf((mask & S_IXOTH) ? "x" : "-");
+    printf("\n");
+
+}
+
+
+mode_t checkFile(char* filename){
+ 
+    struct stat file_info;
+
+    if(stat(filename, &file_info) == 0) {
+        return file_info.st_mode & 0777;
+    }
+
+    return (mode_t)-1;
+
+}
+
+
+mode_t makeMask(char* owner, char* mode){
+
+    mode_t owner_mask = 0;
+    mode_t mode_mask = 0;
+    int i = 0;
+    while(owner[i] != '\0') {
+
+        switch (owner[i]) {
+
+            case 'u':
+
+                owner_mask = owner_mask | S_IRWXU;               
+                break;
+            
+            case 'g':
+
+                owner_mask = owner_mask | S_IRWXG;
+                break;
+
+            case 'o':
+
+                owner_mask = owner_mask | S_IRWXO;
+                break;
+
+            default:
+                break;
+        }
+
+        i++;
+
+    }
+
+    i = 0;
+
+    while(mode[i] != '\0') {
+
+        switch (mode[i]) {
+
+            case 'r':
+                
+                mode_mask = mode_mask | S_IRUSR | S_IRGRP | S_IROTH;
+                break;
+            
+            case 'w':
+
+                mode_mask = mode_mask | S_IWUSR | S_IWGRP | S_IWOTH;
+                break;
+
+            case 'x':
+
+                mode_mask = mode_mask | S_IXUSR | S_IXGRP | S_IXOTH;
+                break;
+
+            default:
+                break;
+        }
+
+        i++;
+
+    }
+
+    return mode_mask & owner_mask;
+
+}
+
+
+mode_t operProc(char oper, mode_t mask, mode_t file_mask){
+
+    switch (oper) {
+
+    case '+':
+
+        return mask | file_mask;
+    
+    case '-':
+
+        return ~ mask & file_mask;
+    
+
+    case '=':
+
+        return mask;
+    
+    default:
+        break;
+    }
+
+    return file_mask;
+
+}
+
+
+int inputParsing(char* input, int counter, char* owner, char* oper, char* mode, char* filename){
+
+    int i = 0;
+    while(input[i] != '+' && input[i] != '-' && input[i] != '=') {
+
+        if(input[i] == 'u' || input[i] == 'g' || input[i] == 'o'){
+            owner[i] = input[i];
+            i++;
+            owner = realloc(owner, sizeof(char)*(i+1));
+        }
+        else {
+            return 1;
+        }
+        
+    }
+    
+    owner[i+1] = '\0';
+
+    if(input[i] == '+' || input[i] == '-' || input[i] == '=') {
+        oper[0] = input[i];
+        i++;
+    }
+    else {
+        return 1;
+    }
+
+    
+    for(int j = 0; input[i] != ' '; j++) {
+
+        if(input[i] == 'r' || input[i] == 'w' || input[i] == 'x') {
+            mode[j] = input[i];
+            i++;
+            mode = realloc(mode, sizeof(char)*(j+1));
+        }
+        else {
+            return 1;
+        }
+
+    }
+
+    mode[i+1] = '\0';
+    i++;
+    counter -= i;
+    int filename_lenth = 0;
+    for(int j = 0; j < counter; j++) {
+
+        filename[j] = input[i];
+        i++;
+        filename = realloc(filename, sizeof(char)*(j+1));
+        filename_lenth = j;
+        
+    }
+    filename[filename_lenth+1] = '\0'; 
+
+    return 0;
+}
+
+
+int main(){
+
+    while(1) {
+
+        char* input = calloc(1 ,sizeof(char));
+        char symbol;
+        int counter = 0;
+        printf("Input command: ");
+
+        while(scanf("%c", &symbol) == 1 && symbol != '\n'){
+            
+            input[counter] = symbol;
+            counter++;
+            input = realloc(input, sizeof(char) * (counter+1));
+
+        }
+
+        char* owner = calloc(1, sizeof(char));
+        char oper = ' ';
+        char* mode = calloc(1, sizeof(char));
+        char* filename = calloc(1, sizeof(char));
+
+        int error = inputParsing(input, counter, owner, &oper, mode, filename);
+        if(error == 1) {
+            printf("Incorrect input\n");
+            continue;
+        }
+
+        mode_t file_mask = checkFile(filename);
+        if(file_mask == (mode_t)-1) {
+            printf("No such file in directory\n");
+        }
+        printf("filemask of %s: ", filename);
+        printMask(file_mask);
+
+        mode_t mask = makeMask(owner, mode);
+        mode_t new_mask = operProc(oper, mask, file_mask);
+        printf("New mask: ");
+        printMask(new_mask);
+    }
+
+    return 0;
+}
--- module2/ext_calc.c
+++ module2/ext_calc.c
@@ -1,21 +1,330 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int charToInt(char data[3]){
+int error = 0;
 
-    int result = 0;
-    for(int i = 0; i < 1; i++){
+float addition(float a, float b){
 
-        result = atoi(data);
+    return a + b;
+
+}
+
+
+float multiplication(float a, float b){
+
+    return a * b;
+
+}
+
+
+float division(float a, float b){
+
+    if (b !=(float)0) {
+        return a / b;
+    }   
+    else{
+        error = 1;
+        return a;
+    }
+    
+
+}
+
+
+float subtraction(float a, float b){
+
+    return a - b;
+
+}
+
+
+int isValidInput(const char *input, int counter) {
+    for (int i = 0; i < counter; i++) {
+        
+        if(input[0] == '?'){
+            return 2;
+        }
+
+        else if(isdigit(input[i])) {
+            continue;
+        }
+        
+        else if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '\n') {
+            continue;
+        }
+
+        else {
+            return 0; 
+        }
+    }
+    return 1; 
+}
+
+
+float charToInt(char* data){
+
+    int result = atoi(data);
+
+    return (float)result;
+}
+
+
+void shiftArr(float* values, char* operators, int oper_size, int num){
+
+    for(int i = num; i < oper_size; i++){
+
+        operators[i] = operators[i+1];
 
     }
 
-    return result;
+    for(int i = num + 1; i < oper_size + 1; i++){
+
+        values[i] = values[i+1];
+        
+    }
+
 }
+
+
+char* prioritiesDist(const char* operators, int oper_size){
+
+    char* priority = calloc(1, sizeof(char));
+    for(int i = 0; i < oper_size; i++){
+        
+        if(operators[i] == '*' || operators[i] == '/'){
+            priority[i] = '2';
+        }
+        else {
+            priority[i] = '1';
+        }
+        
+    }
+    
+    return priority;
+
+}
+
+
+int inputParsing(char* input, int counter, char* operators, float* values){
+
+    int arr_size = 0, values_size = 0, oper_size = 0;; 
+    char* num = calloc(1, sizeof(char));
+
+    for(int i = 0; i < counter; i++){
+
+        num = realloc(num, sizeof(char) * (arr_size+1));
+
+        if (input[i] != '*' && input[i] != '/' && input[i] != '+' && input[i] != '-'){
+            
+            
+            num[arr_size] = input[i];
+            arr_size++;
+        }
+        else{
+
+            float res = charToInt(num);
+            free(num);
+            num = calloc(1, sizeof(char));
+
+            values_size++;
+            arr_size = 0;
+            values = realloc(values, sizeof(float)*values_size);
+            values[values_size-1] = res;
+
+            oper_size++;
+            operators = realloc(operators, sizeof(char)*oper_size);
+            operators[oper_size-1] = input[i];
+
+        }
+
+    }
+
+    float res = charToInt(num);
+    printf("values_size=%d\n", values_size);
+    values = realloc(values, sizeof(float)*(values_size+2));
+    values[values_size] = res;
+
+    for(int i = 0; i < oper_size + 1; i++){
+
+        printf("%c ", operators[i]);
+
+    }
+    
+    printf("\n");
+    
+    for(int i = 0; i < values_size + 1; i++){
+
+        printf("%f ", values[i]);
+
+    }
+
+    printf("\n");
+    printf("pointer in inputParsing is %p\n", values);
+
+    return oper_size;
+
+}
+
+
+int operating(float* values, char* operators, int oper_size, int num){
+    float res;
+    
+    printf("operating with %f and %f\n", values[num], values[num+1]);
+
+    switch (operators[num])
+    {
+        case '*':
+
+            res = multiplication(values[num], values[num+1]);
+            values[num] = res;
+
+            shiftArr(values, operators, oper_size, num);
+
+            oper_size--;
+
+
+            break;
+    
+        case '/':
+
+            res = division(values[num], values[num+1]);
+            values[num] = res;
+
+            shiftArr(values, operators, oper_size, num);
+
+            oper_size--;
+
+            break;
+
+        case '+':
+
+            res = addition(values[num], values[num+1]);
+            values[num] = res;
+
+            shiftArr(values, operators, oper_size, num);
+
+            oper_size--;
+
+            break;
+
+        case '-':
+
+            res = subtraction(values[num], values[num+1]);
+            values[num] = res;
+
+            shiftArr(values, operators, oper_size, num);
+
+            oper_size--;
+
+            break;
+
+        default:
+
+            break;
+
+    }
+
+    printf("result is %f\n",res);
+
+    return oper_size;
+
+}
+
+
+void calculation(float* values, char* operators, char* priority, int oper_size){
+
+    for(int i = 0; i < oper_size; i++){
+       
+        if (priority[i] == '2'){
+            printf("operating with %f and %f\n", values[i], values[i+1]);
+            oper_size = operating(values, operators, oper_size, i);
+            for(int j = i; j < oper_size; j++){
+
+                priority[j] = priority[j+1];
+
+            }
+
+        }
+
+    }
+
+    while(oper_size != 0){
+
+        oper_size = operating(values, operators, oper_size, 0);
+        for(int i = 0; i < oper_size; i++){
+
+            priority[i] = priority[i+1];
+
+        }     
+
+    }
+
+}
+
 
 int main(){
 
-    char data[3] = {'1', '2', '3'};
+    while(1){
 
-    printf("%d", charToInt(data));
+        char* expression = calloc(1, sizeof(char));
+        printf("Input an expression:\n");
 
+        int counter = 0, oper_size = 0;
+        char input;
+        float* values = calloc(1, sizeof(float));
+        char* operators = calloc(1, sizeof(char));
+
+        while(scanf("%c", &input) == 1 && input != '\n'){
+            
+            expression[counter] = input;
+            counter++;
+            expression = realloc(expression, sizeof(char) * (counter+1));
+        }
+
+        int check = isValidInput(expression, counter);
+
+        if (check == 0){
+
+            printf("Incorrect input\n");
+            continue;
+
+        }
+
+        else if(check == 2){
+
+            return 0;
+
+        } 
+
+        else {
+
+            oper_size = inputParsing(expression, counter, operators, values);
+            printf("pointer in main is %p\n", values);
+            for(int i = 0; i < 6; i++){
+                printf("%f ", values[i]);
+            }
+            char* priority = prioritiesDist(operators, oper_size);
+
+            printf("\n");
+            calculation(values, operators, priority, oper_size);
+            if (error == 1){
+
+                printf("Division by zero is not allowed\n");
+                error = 0;
+                continue;
+            }
+
+            else {
+
+                printf("result of expression: %f\n", values[0]);
+                free(values);
+                free(operators);
+                continue;
+
+            }
+        }
+
+    }
+    return 0;
 }
