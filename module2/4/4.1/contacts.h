c866f0dcfc1bc8c4a43e848a2614099c07150a94
OlegDiablo <afanev_maks00@mail.ru>
2025-11-28 15:29:22 +0700

    4.1 makefile

---
+++ module2/4/4.1/contacts.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Contact{
+
+    char name[20];
+    char surname[20];
+    char patronymic[20];
+    char phone_number[5][11];
+    int num_counter;
+    char position[30];
+
+}Contact;
+
+
+Contact* initContact(char* name, char* surname, char* patronymic, char* postition);
+void insertName(Contact*, char*);
+void insertSurname(Contact*, char*);
+void insertPatronymic(Contact*, char*);
+void insertPhoneNumber(Contact*, char*, int);
+void insertPosition(Contact*, char*);
+char* scanInput();
+char* scanChangeInput();
+void printString(char* data);
+void printContact(Contact*);
